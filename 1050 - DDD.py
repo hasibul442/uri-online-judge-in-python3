4a555b087e8fb52928c675d941521ec8c8e5c90a
Md. Hasibul Hasan <hasibulhasan442@gmail.com>
2019-07-15 22:43:02 +0600

    Create      1050 - DDD.py

---
+++ 1050 - DDD.py
@@ -0,0 +1,20 @@
+DDD = int(input())
+
+if(DDD == 61):
+    print("Brasilia")
+elif(DDD == 71):
+    print("Salvador")
+elif(DDD == 11):
+    print("Sao Paulo")
+elif(DDD == 21):
+    print("Rio de Janeiro")
+elif(DDD == 32):
+    print("Juiz de Fora")
+elif(DDD == 19):
+    print("Campinas")
+elif(DDD == 27):
+    print("Vitoria")
+elif(DDD == 31):
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
