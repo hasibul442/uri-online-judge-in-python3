3ed69994b6235bac8cda765272d9155942ef7069
Md. Hasibul Hasan <hasibulhasan442@gmail.com>
2019-07-15 22:58:33 +0600

    Create 1052 - Month.py

---
+++ 1052 - Month.py
@@ -0,0 +1,31 @@
+A = int(input())
+
+if (A==1):
+    print("January")
+elif(A==2):
+    print("February")
+elif(A==3):
+    print("March")
+elif(A==4):
+    print("April")
+elif(A==5):
+    print("May")
+elif(A==6):
+    print("June")
+elif(A==7):
+    print("July")
+elif(A==8):
+    print("August")
+elif(A==9):
+    print("September")
+elif(A==10):
+    print("October")
+elif(A==11):
+    print("November")
+elif(A==12):
+    print("December")
+
+
+
+
+
